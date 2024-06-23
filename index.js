72e7dbc884c8d80fb34325e34603874da04680d3
ContardiC <c.contardi@spacecoding.it>
2024-06-23 16:54:07 +0200

    add db connection

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+// DB data
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book_notes",
+    password: "root",
+    port: 5432,
+});
+// DB connection
+db.connect();
 
 const app = express();
 const port = 3000;
+
 
 app.get("/", (req,res)=>{
     res.send("<h1>Welcome!</h1>");
