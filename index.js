7b5e49fd0339f19170403a27ab1d25166f596ba6
ContardiC <c.contardi@spacecoding.it>
2024-06-24 14:58:42 +0200

    add read notes for a selected book

--- index.js
+++ index.js
@@ -21,21 +21,32 @@ app.use(express.static("public"));
 
 async function getBooks() {
   const result = await db.query(
-    "SELECT b.title, a.fname, a.lname, n.review_text, n.rating  FROM books b JOIN notes n ON b.id = n.id_book JOIN authors a ON b.id_author = a.id"
+    "SELECT b.id, b.title, a.fname, a.lname, n.review_text, n.rating  FROM books b JOIN notes n ON b.id = n.id_book JOIN authors a ON b.id_author = a.id;"
   );
   return result.rows;
+}
+
+async function getBook(id) {
+  const result = await db.query(
+    "SELECT b.title, a.fname, a.lname, n.review_text, n.rating , n.note FROM books b JOIN notes n on b.id = $1 JOIN authors a ON b.id_author = a.id;",
+    [id]
+  );
+  return result.rows[0];
 }
 let books = [];
 
 app.get("/", async (req, res) => {
   books = await getBooks();
-  
-  res.render("index.ejs",{
+  res.render("index.ejs", {
     books: books,
   });
 });
+app.get("/book/:id", async (req, res) => {
+  const id = req.params.id;
+  const book = await getBook(id);
+  res.render("notes.ejs", { book: book });
+});
 
-app.get("");
 app.listen(port, () => {
   console.log(`Server is running on  http://localhost:${port} port. `);
 });
---
+++ queries/dql.sql
@@ -0,0 +1,7 @@
+-- select all the reviewed books
+
+SELECT b.id, b.title, a.fname, a.lname, n.review_text, n.rating  FROM books b JOIN notes n ON b.id = n.id_book JOIN authors a ON b.id_author = a.id;
+
+-- select a book to display notes
+
+SELECT b.title, a.fname, a.lname, n.review_text, n.rating , n.note FROM books b JOIN notes n on b.id = $1 JOIN authors a ON b.id_author = a.id;
